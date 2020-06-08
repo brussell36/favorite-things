cd9c9aab36fca701cfb8b5985f75f49d96a9ae6f
James Henager <jameschenager@gmail.com>
2020-06-08 11:36:42 -0700

    add function to append form data into ul
    Co-Authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,17 @@ $(document).ready(function() {
     let cream = $("#ice-cream").val();
     let animal = $("#animal").val();
     let array = [color, cream, animal];
+    
+    $("li.fave-list").append(
+      "<li>" + array[0] + "</li>",
+      "<li>" + array[1] + "</li>",
+      "<li>" + array[2] + "</li>"
+      );
+
     console.log(array);
+    return array;
   });
 });
+
+
+
